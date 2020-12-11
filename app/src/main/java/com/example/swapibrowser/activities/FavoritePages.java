41f7fb4964345d44761f9c0044ea6dfe93879395
robinettec4 <connor.robinette.1996@gmail.com>
2020-12-10 19:49:29 -0500

    Beginning work on Favorites and Last Visited

---
+++ app/src/main/java/com/example/swapibrowser/activities/FavoritePages.java
@@ -0,0 +1,19 @@
+package com.example.swapibrowser.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.swapibrowser.R;
+
+public class FavoritePages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorites);
+
+
+
+    }
+}
