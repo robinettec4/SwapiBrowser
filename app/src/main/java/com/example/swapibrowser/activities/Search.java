8b31861503bdcdef65c2571f98fbc1ffe80c44a3
robinettec4 <connor.robinette.1996@gmail.com>
2020-12-10 19:49:06 -0500

    Fixed Search

--- app/src/main/java/com/example/swapibrowser/activities/Search.java
+++ app/src/main/java/com/example/swapibrowser/activities/Search.java
@@ -62,21 +62,37 @@ public class Search extends AppCompatActivity {
         setContentView(R.layout.activity_search);
         searchRecycler = findViewById(R.id.search_recycler);
         inputText = findViewById(R.id.input);
+
+        final Spinner recentlyUpdatedSpinner = findViewById(R.id.search_spinner);
+
+        recentlyUpdatedSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                field = recentlyUpdatedSpinner.getSelectedItem().toString().toLowerCase();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     public void search(View view){
+        Log.d("progress", "onClick");
         input = inputText.getText().toString().toLowerCase();
-        searchItems(input);
+        searchItems(field);
     }
 
     private void searchItems(final String itemType) {
+        Log.d("progress", "searchItems");
         final ISearcher searcher = new SearcherFactory().CreateSearcher(itemType);
         final ArrayList<ISingleModel> items = new ArrayList<>();
         final ApiResponseListener<IModel> listener = new ApiResponseListener<IModel>() {
             @Override
             public void onResponseReceived(IModel response) {
+                Log.d("progress", "onResponseReceived");
                 if (response != null) {
-                    items.add((ISingleModel) response.getResults());
+                    items.add((ISingleModel) response.getResults().get(0));
                     save(items.get(0).getUrl(), itemType);
                     searchRecycler.setAdapter(new AdapterFactory().CreateAdapter(itemType.toLowerCase(), items, Search.this));
                     searchRecycler.setLayoutManager(new LinearLayoutManager(Search.this));
@@ -91,6 +107,7 @@ public class Search extends AppCompatActivity {
 
             }
         };
+        Log.d("progress", "getBySearch");
         searcher.getBySearch(input, listener);
     }
 
