176a2dfeb6b8ef2371608bec3340707b3cceb571
Schmooly9 <70486146+Schmooly9@users.noreply.github.com>
2020-12-10 23:56:29 -0500

    Add files via upload

--- app/src/main/java/com/example/swapibrowser/activities/MainActivity.java
+++ app/src/main/java/com/example/swapibrowser/activities/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.swapibrowser.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 
 import com.example.swapibrowser.R;
@@ -18,14 +28,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
     }
 
-    public void goToRandomPageActivity(View view){
+    public void goToRandomPageActivity(View view) {
         Intent intent = new Intent(MainActivity.this, RandomPage.class);
         startActivity(intent);
-      
+        addNotification();
+    }
+
     public void goToRecentlyUpdatedActivity(View view){
         Intent intent = new Intent(MainActivity.this, RecentlyUpdated.class);
         MainActivity.this.startActivity(intent);
+    }
+
+    String CHANNEL_ID = "test channel ID";
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private CharSequence textTitle = "Recommendations for you";
+    private CharSequence textContent = "We think you'll enjoy this page";
+    int notificationId = 123;
+    private void addNotification() {
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, RandomPage.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_RECOMMENDATION)
+        // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(notificationId, builder.build());
+
     }
 }
